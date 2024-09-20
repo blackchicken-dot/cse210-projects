d48706c3b8a7967976e7bc4e284fca57b48a808a
austynn007 <onunkwougochukwu@yahoo.com>
2024-09-20 16:22:48 +0200

    Finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,52 @@
 using System;
 
-class Program
+namespace SimpleFunctions
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            // Call DisplayWelcome function
+            DisplayWelcome();
+
+            // Call PromptUserName function and save return value
+            string userName = PromptUserName();
+
+            // Call PromptUserNumber function and save return value
+            int userNumber = PromptUserNumber();
+
+            // Call SquareNumber function and save return value
+            int squaredNumber = SquareNumber(userNumber);
+
+            // Call DisplayResult function with user name and squared number
+            DisplayResult(userName, squaredNumber);
+        }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string name, int squaredNumber)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+        }
     }
 }
