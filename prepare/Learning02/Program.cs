124644d06e9ee5f4d336cfcaa0576e893b7c8962
austynn007 <onunkwougochukwu@yahoo.com>
2024-09-24 12:04:36 +0200

    Finished learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class Program 
 {
-    static void Main(string[] args)
+    static void Main() 
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume myResume = new Resume();
+        myResume._name = "Ugochukwu Onunkwo";
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Avcera Digital Tech";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "VitalTech";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
